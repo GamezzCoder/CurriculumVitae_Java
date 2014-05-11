f06c862c6cfedd6fb4c19401b7986612c7f94f33
Wolfgang Beer <gamezzcoder@gmail.com>
2014-05-11 20:57:32 +0200

    problems 1-2 solved

--- src/projectEuler/Problem1.java
+++ src/projectEuler/Problem1.java
@@ -4,6 +4,28 @@ public class Problem1 {
 
 	public static void main(String[] args) {
 		
+		//Problem:
+		//If we list all the natural numbers below 10 that are multiples of 3 or 5,
+		//we get 3, 5, 6 and 9. The sum of these multiples is 23.
+		//Find the sum of all the multiples of 3 or 5 below 1000.
+		
+		//Solution: 233168
+		//Time token: 0.001804450760548193 Nanoseconds
+		
+		double startTime = System.nanoTime();
+		double endTime = 0;
+		int solution = 0;
+		
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				solution += i;
+			}
+		}
+		
+		System.out.println(solution);
+		endTime = System.nanoTime();
+		System.out.println("Time token for execution in Nanoseconds: " + (int) endTime/startTime);
+		
 	}
 
 }
